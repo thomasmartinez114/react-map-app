c4d4415ae8f6d8ba739489ea94dc6d4daf734758
Thomas Martinez <devtommie114@gmail.com>
2024-06-28 17:17:57 -0400

    added Markers

--- src/App.js
+++ src/App.js
@@ -2,15 +2,39 @@ import logo from './logo.svg';
 import './App.css';
 import 'leaflet/dist/leaflet.css';
 
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 function App() {
+  const markers = [
+    {
+      geocode: [48.86, 2.3522],
+      popUp: 'Hello, I am popup 1',
+    },
+    {
+      geocode: [48.85, 2.3522],
+      popUp: 'Hello, I am popup 2',
+    },
+    {
+      geocode: [48.855, 2.34],
+      popUp: 'Hello, I am popup 3',
+    },
+  ];
+
+  // providence coordinates
+  // 41.824, 71.4128
+
   return (
-    <MapContainer center={[41.824, 71.4128]} zoom={13}>
+    <MapContainer center={[48.8566, 2.3522]} zoom={13}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
       />
+      {/* <Marker position={[41.824, 71.4128]}>
+        <Popup>My first Marker creation! - devtommie</Popup>
+      </Marker> */}
+      {markers.map(marker => (
+        <Marker position={marker.geocode}></Marker>
+      ))}
     </MapContainer>
   );
 }
